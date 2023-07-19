db1c2f6c2e797e51845eb9464c75aac416fa4632
JohnDev <johncroft2236@hotmail.com>
2023-07-19 18:29:21 -0300

    Implementação e teste da classe Controller

--- src/main/java/com/testinho/pimba/controllers/NpcController.java
+++ src/main/java/com/testinho/pimba/controllers/NpcController.java
@@ -1,16 +1,26 @@
 package com.testinho.pimba.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.testinho.pimba.entities.Npc;
 import com.testinho.pimba.repositories.NpcRepository;
 
-@Controller
-@RequestMapping(path = "/npcs") //
+@Controller // define a classe como Controller
+@RequestMapping(path = "/npcs") // URL irá começar com /npcs
 public class NpcController {
 
-
-    @Autowired //
+    @Autowired 
     private NpcRepository npcRepository;
+
+    @GetMapping
+    public List<Npc> getAll() {
+
+        List<Npc> npcList = npcRepository.findAll();
+        return npcList;
+    }
 }
--- src/main/java/com/testinho/pimba/entities/Npc.java
+++ src/main/java/com/testinho/pimba/entities/Npc.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-@Entity
+@Entity(name = "npc")
+@Table(name = "tb_npc")
 public class Npc {
 
     @Id
